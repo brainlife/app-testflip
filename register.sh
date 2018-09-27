0ce1b644c096f1219370987bb043934b4f3c040e
Soichi Hayashi <hayashis@iu.edu>
2018-09-27 02:28:57 +0000

    added code to clean up bvecs/bvals

--- register.sh
+++ register.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+cat $(jq -r .bvecs config.json) | tr ',' ' ' > bvecs
+cat $(jq -r .bvals config.json) | tr ',' ' ' > bvals
+
 echo "fsl/running bet2"
 bet2 $(jq -r .dwi config.json) dwi.bet -m 
 echo "dtifit to generate FA to register it to FMRIB58_FA_1mm.nii.gz template"
-dtifit -k $(jq -r .dwi config.json) -o dwi.dtifit -m dwi.bet_mask.nii.gz -r $(jq -r .bvecs config.json) -b $(jq -r .bvals config.json) -w 
+dtifit -k $(jq -r .dwi config.json) -o dwi.dtifit -m dwi.bet_mask.nii.gz -r bvecs -b bvals -w 
 flirt -in dwi.dtifit_FA.nii.gz -ref $FSLDIR/data/standard/FMRIB58_FA_1mm.nii.gz -omat fa.xfm -dof 12 
 
 echo "converting from 12 to 6 dof"
